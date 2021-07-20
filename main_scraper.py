03bc2f708ea9b517f97f20ed0ed527b6e1b1f5c5
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-07-20 06:42:19 +0300

    Add Class Data and get_data

--- main_scraper.py
+++ main_scraper.py
@@ -9,10 +9,23 @@ API_KEY = os.environ.get('API_KEY')
 PROJECT_TOKEN = os.environ.get("PROJECT_TOKEN")
 RUN_TOKEN = "RUN_TOKEN"
 
-response = requests.get(
-    f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data',
-    params={'api_key': API_KEY})
 
-data = json.loads(response.text)
+class Data:
+    def __init__(self, api_key, project_token):
+        self.api_key = api_key
+        self.project_token = project_token
+        self.params = {
+            "api_key": self.api_key
+        }
+        self.get_data()
 
-print(data)
+    def get_data(self):
+        response = requests.get(
+            f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data',
+            params={'api_key': API_KEY})
+        self.data = json.loads(response.text)
+        # print(data['total'])
+
+
+data = Data(API_KEY, PROJECT_TOKEN)
+print(data.data['total'])
