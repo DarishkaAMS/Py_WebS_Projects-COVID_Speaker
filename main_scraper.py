3b332c739c873a389c03d77baf70aabeeb5f2a77
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-07-20 06:35:31 +0300

    Add data response

--- main_scraper.py
+++ main_scraper.py
@@ -1,13 +1,18 @@
-import requests
-
 from dotenv import load_dotenv
+import json
+import os
+import requests
 
 load_dotenv()
 
-API_KEY = "API_KEY"
-PROJECT_TOKEN = "PROJECT_TOKEN"
+API_KEY = os.environ.get('API_KEY')
+PROJECT_TOKEN = os.environ.get("PROJECT_TOKEN")
 RUN_TOKEN = "RUN_TOKEN"
 
 response = requests.get(
     f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data',
     params={'api_key': API_KEY})
+
+data = json.loads(response.text)
+
+print(data)
