60962e4e3b4a158b916f1755c27ae16ab29176c6
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-07-20 06:57:12 +0300

    Add get_total_cases, get_total_cases and get_country_data

--- main_scraper.py
+++ main_scraper.py
@@ -26,6 +26,31 @@ class Data:
         self.data = json.loads(response.text)
         # print(data['total'])
 
+    def get_total_cases(self):
+        data = self.data['total']
+
+        for content in data:
+            if content['name'] == 'Coronavirus Cases:':
+                return content['value']
+
+    def get_total_cases(self):
+        data = self.data['total']
+
+        for content in data:
+            if content['name'] == 'Deaths:':
+                return content['value']
+
+        return "Hmmm... I have not found anything with these criteria... Please try again"
+
+    def get_country_data(self, country):
+        data = self.data['country']
+
+        for content in data:
+            if content['name'].lower() == country.lower():
+                return content
+        return "Hmmm... I have not found anything with these criteria... Please try again"
+
 
 data = Data(API_KEY, PROJECT_TOKEN)
-print(data.data['total'])
+print(data.get_country_data("Ukraine")['total_cases'])
+print(data.get_country_data("Ukraine"))
